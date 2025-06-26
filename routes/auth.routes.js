0f5bd93ee51a7422b0ab04a5f22e494396963830
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-06-27 01:13:56 +0530

    Update auth.routes.js added otp verification

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
-import { login, register } from '../controllers/auth.controllers.js';
+import { login, register,verifyOtp } from '../controllers/auth.controllers.js';
 import express from "express"
 export const authrouter=express.Router();
 
 authrouter.post('/register',register)
 authrouter.post('/login',login)
+authrouter.post('/verifyotp',verifyOtp)
