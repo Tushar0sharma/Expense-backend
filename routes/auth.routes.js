bb8bf8185d1a29f2ad96ab11746c05177124e381
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2025-06-30 00:08:31 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,9 @@
-import { login, register,verifyOtp } from '../controllers/auth.controllers.js';
+import { forgotpass, login, register,setpass,verifyOtp } from '../controllers/auth.controllers.js';
 import express from "express"
 export const authrouter=express.Router();
 
 authrouter.post('/register',register)
 authrouter.post('/login',login)
 authrouter.post('/verifyotp',verifyOtp)
+authrouter.post('/forgotpass',forgotpass)
+authrouter.post('/setpass',setpass)
